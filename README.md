b3629c418a95d6a3093d6001dde4ca1a42b338fc
Jacek Szulc <j.szulc77@wp.pl>
2024-11-11 00:42:09 +0100

    Projekt#22.3

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Projekt_22_Formularz_kontaktowy
+## Konfiguracja w c:\xampp
+```
+[php.ini]
+SMTP=smtp.gmail.com
+smtp_port=587
+sendmail_from = abc@gmail.com
+sendmail_path = "\"C:\xampp\sendmail\sendmail.exe\" -t"
+
+[sendmail.ini]
+smtp_server=smtp.gmail.com
+smtp_port=587
+error_logfile=error.log
+debug_logfile=debug.log
+auth_username=abc@gmail.com
+auth_password=****TAJNE****
+force_sender=abc@gmail.com
+```
--- mail.php
+++ mail.php
@@ -2,19 +2,23 @@
 
 $name = $_POST["name"];
 $from = $_POST["email"];
-$subject = "Wiadomość z formularza";
+$subject = "/PHP E-MAIL SENDER/ - Wiadomość z formularza";
 $to = "jacekpa23143@gmail.com";
 $message = $_POST["msg"];
 
-$txt = "Imię " . $name . "\r\n" . "Email: " .$from . "\r\n" . "\r\n" . "Treść: " . $message;
+$txt = "Imię: " . $name . "\r\n" . "Email: " .$from . "\r\n" . "\r\n" . "Treść: " . $message;
 
+$headers = 'Content-Type: text/plain; charset=utf-8' . "\r\n";
+$headers .= 'Content-Transfer-Encoding: base64' . "\r\n";
 $headers = "From: " . $from . "\r\n";
 $headers .= "Reply-To: " . $from . "\r\n";
 
-$mail_ststus = mail($to, $subject, $message, $headers);
+$mail_status = mail($to, $subject, $message, $headers);
 
 if($mail_status) {
     header("Location: index.html?mail_status=sent");
 } else {
     header("Location: index.html?mail_status=error");
 }
+
+?>
